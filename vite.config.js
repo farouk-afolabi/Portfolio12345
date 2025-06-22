d511ea316650b91a63131e0bc2dfe9a20e1e1a49
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-06-22 01:56:07 -0400

    added CNAME file for custom domain persistence & update vite config for domain persistence

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // For GitHub Pages  use '/Portfolio12345/'
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true // Ensures CNAME is preserved
+  }
 })
